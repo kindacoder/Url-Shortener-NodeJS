118def267048e9869395bc3e80b9fac405992816
kindacoder <code.ashutosh@gmail.com>
2018-02-03 01:17:59 +0530

    Final commit and Woohoo Shortner is working fine

--- encoder_decoder/encode.js
+++ encoder_decoder/encode.js
@@ -2,7 +2,7 @@
 var encode = function(url) {
     //encode the url- generate the random number and add some integer to them
     //get random Character from url;
-    var randomCharacter = ((url.split('.')[1])[0]) + ((url.split('//')[1])[2]);
+    var randomCharacter = ((url.split('.')[1])[0]) + ((url.split('.')[1])[1]);
     var randomNumber =  Math.floor((Math.random() * 100) + 3);
     return randomCharacter + randomNumber;
 }
