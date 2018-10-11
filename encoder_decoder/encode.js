d64a17dadbd2d868af564edb12e802f28b897590
kindacoder <code.ashutosh@gmail.com>
2018-10-11 14:04:09 +0530

    Fixed all the issue

--- encoder_decoder/encode.js
+++ encoder_decoder/encode.js
@@ -1,10 +1,8 @@
 ///generating the sorten url;
 var hasha=require('hasha');
-var sha1 = require('sha1');
 var encode = function(url) {
   ///using hasha to generate unique 3 digit letter:)
 var hash=hasha(url);
-var hash2=sha1(url);
 ///randomly choose 4 letters on hash by generating a number;
 var loop=3;
 var randomNumber;
